01d18fe46402f783b8fdd48c79b73e7815903ab2
nico-develop <contact@nico-develop.fr>
2022-10-07 09:07:41 +0200

    Initialisation du projet et intégration d'une fonction de création d'utilisateur

---
+++ functions/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'root': true,
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+  },
+};
---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.createUser = functions.https.onCall((data, context) => {
+  return admin.auth().createUser(data);
+});
+
